58cd3c862ddb569492e75c726a6e6b777255ed79
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-28 17:25:07 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,14 +14,23 @@ const codes = [
 function init() {
 
   // Attaching an keydown event listener to document.body
-  document.body.addEventListener("keydown", (event) => {
+  document.body.addEventListener("keydown", onKeyDownHandler(e) => {
     // Now, how can we check for which specific key was pressed?
-var n = []
-for(let i = 0; i < codes.length; i++) {
-  n.push(e.key[i])
-}
-  if(n === codes) {
-    alert("Hurray!")
+    function onKeyDownHandler(e) {
+      const key = e.key;
+     
+      if (key === codes[index]) {
+        index++;
+     
+        if (index === codes.length) {
+          alert("Hurray!");
+     
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    }
   }
   })
 }
