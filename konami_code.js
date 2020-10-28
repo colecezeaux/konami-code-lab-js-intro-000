2340e067038834af741bda46cb0c1eaea2b6d49d
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-28 16:03:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,8 +15,6 @@ var index = 0;
 
 function init() {
   document.body.addEventListener("keydown", (event) => {
-    let key = e.keydown;
-
     for(let i = 0, l = codes.length; i < l; i++) {
     if(codes[i] === l) {
       alert("Hurray!");
