ee9acf4f899fca7d3a25a86d68e74632e066a12d
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-28 17:33:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -20,14 +20,9 @@ function init() {
 
       if (key === codes[index]) {
         index++;
-
+      }
         if (index === codes.length) {
           alert("Hurray!");
-
-          index = 0;
         }
-      } else {
-        index = 0;
-  }
   })
 }
